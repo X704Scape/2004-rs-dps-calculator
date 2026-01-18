0ec0f632401acabf4404043f3969d463fcfa58ab
jayhawk225 <jayhawk225aaa@gmail.com>
2026-01-18 17:18:10 +0000

    File changes

---
+++ functions/weaponsMetaon.json.ts
@@ -0,0 +1,314 @@
+[
+    {
+        "id": 35,
+        "name": "Excalibur",
+        "category": "longsword",
+        "speedTicks": 5,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "slash"
+            },
+            {
+                "mode": "aggressive",
+                "id": "aggressive",
+                "type": "slash"
+            },
+            {
+                "mode": "controlled",
+                "id": "controlled",
+                "type": "stab"
+            },
+            {
+                "mode": "defensive",
+                "id": "defensive",
+                "type": "slash"
+            }
+        ]
+    },
+    {
+        "id": 466,
+        "name": "Pickaxe handle",
+        "category": "longsword",
+        "speedTicks": 5,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "slash"
+            },
+            {
+                "mode": "aggressive",
+                "id": "aggressive",
+                "type": "slash"
+            },
+            {
+                "mode": "controlled",
+                "id": "controlled",
+                "type": "stab"
+            },
+            {
+                "mode": "defensive",
+                "id": "defensive",
+                "type": "slash"
+            }
+        ]
+    },
+    {
+        "id": 767,
+        "name": "Phoenix crossbow",
+        "category": "crossbow",
+        "speedTicks": 6,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "ranged"
+            },
+            {
+                "mode": "rapid",
+                "id": "rapid",
+                "type": "ranged"
+            },
+            {
+                "mode": "defensive",
+                "id": "longrange",
+                "type": "ranged"
+            }
+        ],
+        "speedOverrides": [
+            {
+                "styleId": "rapid",
+                "speedTicks": 5
+            }
+        ]
+    },
+    {
+        "id": 772,
+        "name": "Dramen staff",
+        "category": "staff",
+        "speedTicks": 5,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "crush"
+            },
+            {
+                "mode": "aggressive",
+                "id": "aggressive",
+                "type": "crush"
+            },
+            {
+                "mode": "defensive",
+                "id": "defensive",
+                "type": "crush"
+            }
+        ],
+        "supportsSpells": true
+    },
+    {
+        "id": 4,
+        "name": "Crossbow",
+        "category": "crossbow",
+        "speedTicks": 6,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "ranged"
+            },
+            {
+                "mode": "rapid",
+                "id": "rapid",
+                "type": "ranged"
+            },
+            {
+                "mode": "defensive",
+                "id": "longrange",
+                "type": "ranged"
+            }
+        ],
+        "speedOverrides": [
+            {
+                "styleId": "rapid",
+                "speedTicks": 5
+            }
+        ]
+    },
+    {
+        "id": 27,
+        "name": "Dart",
+        "category": "dart",
+        "speedTicks": 3,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "ranged"
+            },
+            {
+                "mode": "rapid",
+                "id": "rapid",
+                "type": "ranged"
+            },
+            {
+                "mode": "defensive",
+                "id": "longrange",
+                "type": "ranged"
+            }
+        ],
+        "speedOverrides": [
+            {
+                "styleId": "rapid",
+                "speedTicks": 2
+            }
+        ]
+    },
+    {
+        "id": 28,
+        "name": "Knife",
+        "category": "knife",
+        "speedTicks": 4,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "ranged"
+            },
+            {
+                "mode": "rapid",
+                "id": "rapid",
+                "type": "ranged"
+            },
+            {
+                "mode": "defensive",
+                "id": "longrange",
+                "type": "ranged"
+            }
+        ],
+        "speedOverrides": [
+            {
+                "styleId": "rapid",
+                "speedTicks": 3
+            }
+        ]
+    },
+    {
+        "id": 42,
+        "name": "Javelin",
+        "category": "javelin",
+        "speedTicks": 5,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "ranged"
+            },
+            {
+                "mode": "rapid",
+                "id": "rapid",
+                "type": "ranged"
+            },
+            {
+                "mode": "defensive",
+                "id": "longrange",
+                "type": "ranged"
+            }
+        ],
+        "speedOverrides": [
+            {
+                "styleId": "rapid",
+                "speedTicks": 4
+            }
+        ]
+    },
+    {
+        "id": 43,
+        "name": "Thrownaxe",
+        "category": "thrownaxe",
+        "speedTicks": 5,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "ranged"
+            },
+            {
+                "mode": "rapid",
+                "id": "rapid",
+                "type": "ranged"
+            },
+            {
+                "mode": "defensive",
+                "id": "longrange",
+                "type": "ranged"
+            }
+        ],
+        "speedOverrides": [
+            {
+                "styleId": "rapid",
+                "speedTicks": 4
+            }
+        ]
+    },
+    {
+        "id": 805,
+        "name": "Longbow",
+        "category": "longbow",
+        "speedTicks": 6,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "ranged"
+            },
+            {
+                "mode": "rapid",
+                "id": "rapid",
+                "type": "ranged"
+            },
+            {
+                "mode": "defensive",
+                "id": "longrange",
+                "type": "ranged"
+            }
+        ],
+        "speedOverrides": [
+            {
+                "styleId": "rapid",
+                "speedTicks": 5
+            }
+        ]
+    },
+    {
+        "id": 806,
+        "name": "Shortbow",
+        "category": "shortbow",
+        "speedTicks": 5,
+        "attackStyles": [
+            {
+                "mode": "accurate",
+                "id": "accurate",
+                "type": "ranged"
+            },
+            {
+                "mode": "rapid",
+                "id": "rapid",
+                "type": "ranged"
+            },
+            {
+                "mode": "defensive",
+                "id": "longrange",
+                "type": "ranged"
+            }
+        ],
+        "speedOverrides": [
+            {
+                "styleId": "rapid",
+                "speedTicks": 4
+            }
+        ]
+    }
+]
