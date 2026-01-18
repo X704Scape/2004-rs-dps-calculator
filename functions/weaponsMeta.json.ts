fc7a68ea747283f3f82635901831030b6dc4f9d1
jayhawk225 <jayhawk225aaa@gmail.com>
2026-01-18 05:00:30 +0000

    File changes

--- functions/weaponsMeta.json.ts
+++ functions/weaponsMeta.json.ts
@@ -1,40 +1,307 @@
 [
-  {
-    "id": 35,
-    "name": "Excalibur",
-    "category": "longsword",
-    "speedTicks": 5,
-    "attackStyles": [
-      { "mode": "accurate", "id": "accurate", "type": "slash" },
-      { "mode": "aggressive", "id": "aggressive", "type": "slash" },
-      { "mode": "controlled", "id": "controlled", "type": "stab" },
-      { "mode": "defensive", "id": "defensive", "type": "slash" }
-    ]
-  },
-  {
-    "id": 767,
-    "name": "Phoenix crossbow",
-    "category": "longbow",
-    "speedTicks": 6,
-    "attackStyles": [
-      { "mode": "accurate", "id": "accurate", "type": "ranged" },
-      { "mode": "rapid", "id": "rapid", "type": "ranged" },
-      { "mode": "defensive", "id": "longrange", "type": "ranged" }
-    ],
-    "speedOverrides": [
-      { "styleId": "rapid", "speedTicks": 5 }
-    ]
-  },
-  {
-    "id": 772,
-    "name": "Dramen staff",
-    "category": "staff",
-    "speedTicks": 5,
-    "attackStyles": [
-      { "mode": "accurate", "id": "accurate", "type": "crush" },
-      { "mode": "aggressive", "id": "aggressive", "type": "crush" },
-      { "mode": "defensive", "id": "defensive", "type": "crush" }
-    ],
-    "supportsSpells": true
-  }
+    {
+        "id":  35,
+        "name":  "Excalibur",
+        "category":  "longsword",
+        "speedTicks":  5,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "aggressive",
+                                 "id":  "aggressive",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "controlled",
+                                 "id":  "controlled",
+                                 "type":  "stab"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "defensive",
+                                 "type":  "slash"
+                             }
+                         ]
+    },
+    {
+        "id":  466,
+        "name":  "Pickaxe handle",
+        "category":  "longsword",
+        "speedTicks":  5,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "aggressive",
+                                 "id":  "aggressive",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "controlled",
+                                 "id":  "controlled",
+                                 "type":  "stab"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "defensive",
+                                 "type":  "slash"
+                             }
+                         ]
+    },
+    {
+        "id":  468,
+        "name":  "Broken pickaxe",
+        "category":  "longsword",
+        "speedTicks":  5,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "aggressive",
+                                 "id":  "aggressive",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "controlled",
+                                 "id":  "controlled",
+                                 "type":  "stab"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "defensive",
+                                 "type":  "slash"
+                             }
+                         ]
+    },
+    {
+        "id":  470,
+        "name":  "Broken pickaxe",
+        "category":  "longsword",
+        "speedTicks":  5,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "aggressive",
+                                 "id":  "aggressive",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "controlled",
+                                 "id":  "controlled",
+                                 "type":  "stab"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "defensive",
+                                 "type":  "slash"
+                             }
+                         ]
+    },
+    {
+        "id":  472,
+        "name":  "Broken pickaxe",
+        "category":  "longsword",
+        "speedTicks":  5,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "aggressive",
+                                 "id":  "aggressive",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "controlled",
+                                 "id":  "controlled",
+                                 "type":  "stab"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "defensive",
+                                 "type":  "slash"
+                             }
+                         ]
+    },
+    {
+        "id":  474,
+        "name":  "Broken pickaxe",
+        "category":  "longsword",
+        "speedTicks":  5,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "aggressive",
+                                 "id":  "aggressive",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "controlled",
+                                 "id":  "controlled",
+                                 "type":  "stab"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "defensive",
+                                 "type":  "slash"
+                             }
+                         ]
+    },
+    {
+        "id":  476,
+        "name":  "Broken pickaxe",
+        "category":  "longsword",
+        "speedTicks":  5,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "aggressive",
+                                 "id":  "aggressive",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "controlled",
+                                 "id":  "controlled",
+                                 "type":  "stab"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "defensive",
+                                 "type":  "slash"
+                             }
+                         ]
+    },
+    {
+        "id":  478,
+        "name":  "Broken pickaxe",
+        "category":  "longsword",
+        "speedTicks":  5,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "aggressive",
+                                 "id":  "aggressive",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "controlled",
+                                 "id":  "controlled",
+                                 "type":  "stab"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "defensive",
+                                 "type":  "slash"
+                             }
+                         ]
+    },
+    {
+        "id":  492,
+        "name":  "Axe handle",
+        "category":  "longsword",
+        "speedTicks":  5,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "aggressive",
+                                 "id":  "aggressive",
+                                 "type":  "slash"
+                             },
+                             {
+                                 "mode":  "controlled",
+                                 "id":  "controlled",
+                                 "type":  "stab"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "defensive",
+                                 "type":  "slash"
+                             }
+                         ]
+    },
+    {
+        "id":  767,
+        "name":  "Phoenix crossbow",
+        "category":  "longbow",
+        "speedTicks":  6,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "ranged"
+                             },
+                             {
+                                 "mode":  "rapid",
+                                 "id":  "rapid",
+                                 "type":  "ranged"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "longrange",
+                                 "type":  "ranged"
+                             }
+                         ],
+        "speedOverrides":  [
+                             {
+                                 "styleId":  "rapid",
+                                 "speedTicks":  5
+                             }
+                         ]
+    },
+    {
+        "id":  772,
+        "name":  "Dramen staff",
+        "category":  "staff",
+        "speedTicks":  5,
+        "attackStyles":  [
+                             {
+                                 "mode":  "accurate",
+                                 "id":  "accurate",
+                                 "type":  "crush"
+                             },
+                             {
+                                 "mode":  "aggressive",
+                                 "id":  "aggressive",
+                                 "type":  "crush"
+                             },
+                             {
+                                 "mode":  "defensive",
+                                 "id":  "defensive",
+                                 "type":  "crush"
+                             }
+                         ],
+        "supportsSpells":  true
+    }
 ]
