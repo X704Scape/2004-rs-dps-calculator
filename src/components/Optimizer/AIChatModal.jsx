ab55868b0d795c7d85ec78b206f34fa7849b2ec9
jayhawk225 <jayhawk225aaa@gmail.com>
2026-03-15 06:20:07 +0000

    File changes

--- src/components/Optimizer/AIChatModal.jsx
+++ src/components/Optimizer/AIChatModal.jsx
@@ -382,7 +382,7 @@ export default function AIChatModal({ playerStats, monster, availableMonsters, l
         {/* Messages */}
         <div className="flex-1 overflow-y-auto p-4">
           {messages.map((msg, i) => (
-            <ChatMessage key={i} msg={msg} onApply={handleInteraction} availableLoadouts={loadouts} />
+            <ChatMessage key={i} msg={msg} onApply={handleInteraction} onCreateAndApply={handleCreateAndApply} availableLoadouts={loadouts} />
           ))}
           {loading && (
             <div className="flex justify-start mb-3">
